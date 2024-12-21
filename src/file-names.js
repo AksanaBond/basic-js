371b68485bad816048076a82ae2effab5fce9d9c
AksanaBond <oksana@MacBook-Air-Oksana.local>
2024-12-21 21:37:06 +0100

    fix: pass dns-stats, dream-team, file-names tasks

--- src/file-names.js
+++ src/file-names.js
@@ -15,9 +15,19 @@ const { NotImplementedError } = require('../extensions/index.js');
  * the output should be ["file", "file(1)", "image", "file(1)(1)", "file(2)"]
  *
  */
-function renameFiles(/* names */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function renameFiles(names) {
+  names.forEach((name, i) => {
+    const itemsBeforeCurrent = i === 0 ? [] : names.slice(0, i);
+    let nameWithSuffix = name;
+    let suffixIndex = 0;
+    while (itemsBeforeCurrent.includes(nameWithSuffix)) {
+      suffixIndex ++;
+      nameWithSuffix = `${name}(${suffixIndex})`
+    }
+    names[i] = nameWithSuffix;
+  })
+
+  return names;
 }
 
 module.exports = {
